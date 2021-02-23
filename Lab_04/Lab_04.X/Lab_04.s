a16b6ef9a40f22da7174423afb7885ca7448074d
god19260 <68131294+god19260@users.noreply.github.com>
2021-02-23 04:27:04 -0600

    .

--- Lab_04/Lab_04.X/Lab_04.s
+++ Lab_04/Lab_04.X/Lab_04.s
@@ -8,7 +8,7 @@ processor 16F887
  
 ; CONFIG1
   CONFIG  FOSC = INTRC_NOCLKOUT ; Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
-  CONFIG  WDTE = ON             ; Watchdog Timer Enable bit (WDT enabled)
+  CONFIG  WDTE = OFF             ; Watchdog Timer Enable bit (WDT enabled)
   CONFIG  PWRTE = OFF           ; Power-up Timer Enable bit (PWRT disabled)
   CONFIG  MCLRE = OFF           ; RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
   CONFIG  CP = OFF              ; Code Protection bit (Program memory code protection is disabled)
@@ -23,6 +23,19 @@ processor 16F887
   CONFIG  WRT = OFF             ; Flash Program Memory Self Write Enable bits (Write protection off)
     
 ;---------------------------------------------------------
+;--------------- Macros ----------------------------------
+  
+Incrementar macro Boton, puerto   ; Incrimenta con configuración pullup 
+  btfsc  Boton
+  incf   puerto,1
+  endm
+  
+Decrementar macro Boton, puerto   ; Decrementa con configuración pullup 
+  btfsc  Boton
+  decf   puerto,1
+  endm
+  
+;---------------------------------------------------------
 ;------------ Variables a usar----------------------------
     ;------- Nombrar Pines ---------  
 B_Inc   EQU  0
@@ -33,6 +46,7 @@ PSECT udata_bank0
 ;PSECT udata_shr  ; common memory
     W_TEMP:      DS 1
     STATUS_TEMP: DS 1
+    Cont:        DS 1
     
     
 ;---------------------------------------------------------
@@ -44,7 +58,7 @@ resVect:
     goto    main 
     
 ;---------------------------------------------------------
-;------------ Interrupt Vector ---------------------------
+;------------ Interrupción ---------------------------
 PSECT resVect, class=code, abs, delta=2  
 ORG 04h
 push:
@@ -52,30 +66,68 @@ push:
     swapf  STATUS,W
     movwf  STATUS_TEMP
 isr:
-    btfsc  RBIF
-    call   Interrupcion
+    btfsc  T0IF
+    incf   PORTD
+    
+    btfss  PORTB, B_Inc
+    call   Incremento
+    btfss  PORTB, B_Dec
+    call   Decremento
+    bcf    RBIF
+    movf  PORTA,0
+    call  Display
+    movwf PORTC 
+    ;bcf    T0IF
+    
 pop: 
     swapf  STATUS_TEMP,W
     movwf  STATUS
     swapf  W_TEMP, F
     swapf  W_TEMP, W
-    retfie
+    RETFIE
     
-Interrupcion:
+Incremento:
     btfss    PORTB, B_Inc
-    incf     PORTA, 1
+    goto     $-1
+    incf     PORTA
+    return
+Decremento:
     btfss    PORTB, B_Dec
-    decf     PORTA, 1
-    bcf      RBIF
+    goto     $-1
+    decf     PORTA
     return
 ;---------------------------------------------------------
 ;------------ Definición del Inicio ----------------------
 PSECT code, delta=2, abs
 ORG 100h
-
+;---------------------------------------------------------
+;------------ Tablas -------------------------------------
+Display:
+    clrf  PCLATH
+    bsf   PCLATH,0
+    andlw 0x0F
+    addwf PCL
+    retlw 00111111B ; Cero
+    retlw 00000110B ;Uno
+    retlw 01011011B ;Dos
+    retlw 01001111B ;Tres
+    retlw 01100110B ;Cuatro
+    retlw 01101101B ;Cinco
+    retlw 01111101B ;Seis 
+    retlw 00000111B ;Siete
+    retlw 01111111B ;Ocho
+    retlw 01100111B ;Nueve
+    retlw 01110111B ;A
+    retlw 01111100B ;B
+    retlw 00111001B ;C
+    retlw 01011110B ;D
+    retlw 01111001B ;E
+    retlw 01110001B ;F 
+ 
 ;---------------------------------------------------------
 ;------------ Main ---------------------------------------
 main: 
+    
     ;------- Configuraciones -------
     BANKSEL  OSCCON
     bcf      IRCF0       ; Donfiguración del reloj interno 
@@ -86,36 +138,55 @@ main:
     clrf     ANSEL
     clrf     ANSELH
     
+    ; ---------- Activar pines como salidas o entradas
     banksel  TRISA         
     bsf      TRISB, B_Inc  ; Colocar los pines B_Inc y B_Dec como entradas
     bsf      TRISB, B_Dec
+    movlw    11110000B     ; PORTA 0 al 3 como salidas y 4 al 7 como entradas
+    movwf    TRISA
+    movlw    10000000B     ; PORTC 0 al 6 como salidas y el 7 como entrada
+    movwf    TRISC
+    movlw    10000000B     ; PORTD 0 al 6 como salidas y el 7 como entrada
+    movwf    TRISD
     
     banksel  OPTION_REG
     bcf      OPTION_REG, 7
-    bsf      WPUB, B_Inc  ; Activar los pullups de los pines B_Inc y B_Dec 
-    bsf      WPUB, B_Dec
+    bsf      WPUB, B_Inc   ; Activar los pullups de los pines B_Inc y B_Dec 
+    bsf      WPUB, B_Dec 
+    
+    
+    bsf      OPTION_REG, 0     ; Se selecciona un preescaler de 256
+    bsf      OPTION_REG, 1
+    bsf      OPTION_REG, 2
+    
     
     banksel  IOCB
     bsf      IOCB, B_Inc   ; Habilitar Interrupt on change en B_Inc y B_Dec
     bsf      IOCB, B_Dec  
     
-    banksel  PORTA
-    movf     PORTB, W
+    ;banksel  PORTA
+   ; movf     PORTB, W
  
     
     banksel  INTCON
-    movlw    10001000B
+    movlw    11101000B
     movwf    INTCON,1
     ;bsf      GIE
     ;bsf      RBIE
     ;bcf      RBIF
     
-    
+    banksel  PORTA
+    clrf     PORTA
     clrf     PORTB
+    clrf     PORTC
+    clrf     PORTD
+    clrf     TMR0
 ;---------------------------------------------------------
 ;----------- Loop Forever --------------------------------    
-loop:    
-    
-    goto loop
+loop:   
+  
+    goto  loop
+ 
+    ;   11101101B     ; 237
 
 end 
--- Lab_04/Lab_04.X/build/default/production/Lab_04.i
+++ Lab_04/Lab_04.X/build/default/production/Lab_04.i
@@ -2457,7 +2457,7 @@ ENDM
 
 ; CONFIG1
   CONFIG FOSC = INTRC_NOCLKOUT ; Oscillator Selection bits (INTOSCIO oscillator: I/O function on ((PORTA) and 07Fh), 6/OSC2/CLKOUT pin, I/O function on ((PORTA) and 07Fh), 7/OSC1/CLKIN)
-  CONFIG WDTE = ON ; Watchdog Timer Enable bit (WDT enabled)
+  CONFIG WDTE = OFF ; Watchdog Timer Enable bit (WDT enabled)
   CONFIG PWRTE = OFF ; Power-up Timer Enable bit (PWRT disabled)
   CONFIG MCLRE = OFF ; ((PORTE) and 07Fh), 3/MCLR pin function select bit (((PORTE) and 07Fh), 3/MCLR pin function is MCLR)
   CONFIG CP = OFF ; Code Protection bit (Program memory code protection is disabled)
@@ -2472,6 +2472,19 @@ ENDM
   CONFIG WRT = OFF ; Flash Program Memory Self Write Enable bits (Write protection off)
 
 ;---------------------------------------------------------
+;--------------- Macros ----------------------------------
+
+Incrementar macro Boton, puerto ; Incrimenta con configuración pullup
+  btfsc Boton
+  incf puerto,1
+  endm
+
+Decrementar macro Boton, puerto ; Decrementa con configuración pullup
+  btfsc Boton
+  decf puerto,1
+  endm
+
+;---------------------------------------------------------
 ;------------ Variables a usar----------------------------
     ;------- Nombrar Pines ---------
 B_Inc EQU 0
@@ -2482,6 +2495,7 @@ PSECT udata_bank0
 ;PSECT udata_shr ; common memory
     W_TEMP: DS 1
     STATUS_TEMP: DS 1
+    Cont: DS 1
 
 
 ;---------------------------------------------------------
@@ -2493,7 +2507,7 @@ resVect:
     goto main
 
 ;---------------------------------------------------------
-;------------ Interrupt Vector ---------------------------
+;------------ Interrupción ---------------------------
 PSECT resVect, class=code, abs, delta=2
 ORG 04h
 push:
@@ -2501,30 +2515,68 @@ push:
     swapf STATUS,W
     movwf STATUS_TEMP
 isr:
-    btfsc ((INTCON) and 07Fh), 0
-    call Interrupcion
+    btfsc ((INTCON) and 07Fh), 2
+    incf PORTD
+
+    btfss PORTB, B_Inc
+    call Incremento
+    btfss PORTB, B_Dec
+    call Decremento
+    bcf ((INTCON) and 07Fh), 0
+    movf PORTA,0
+    call Display
+    movwf PORTC
+    ;bcf ((INTCON) and 07Fh), 2
+
 pop:
     swapf STATUS_TEMP,W
     movwf STATUS
     swapf W_TEMP, F
     swapf W_TEMP, W
-    retfie
+    RETFIE
 
-Interrupcion:
+Incremento:
     btfss PORTB, B_Inc
-    incf PORTA, 1
+    goto $-1
+    incf PORTA
+    return
+Decremento:
     btfss PORTB, B_Dec
-    decf PORTA, 1
-    bcf ((INTCON) and 07Fh), 0
+    goto $-1
+    decf PORTA
     return
 ;---------------------------------------------------------
 ;------------ Definición del Inicio ----------------------
 PSECT code, delta=2, abs
 ORG 100h
+;---------------------------------------------------------
+;------------ Tablas -------------------------------------
+Display:
+    clrf PCLATH
+    bsf PCLATH,0
+    andlw 0x0F
+    addwf PCL
+    retlw 00111111B ; Cero
+    retlw 00000110B ;Uno
+    retlw 01011011B ;Dos
+    retlw 01001111B ;Tres
+    retlw 01100110B ;Cuatro
+    retlw 01101101B ;Cinco
+    retlw 01111101B ;Seis
+    retlw 00000111B ;Siete
+    retlw 01111111B ;Ocho
+    retlw 01100111B ;Nueve
+    retlw 01110111B ;A
+    retlw 01111100B ;B
+    retlw 00111001B ;C
+    retlw 01011110B ;D
+    retlw 01111001B ;E
+    retlw 01110001B ;F
 
 ;---------------------------------------------------------
 ;------------ Main ---------------------------------------
 main:
+
     ;------- Configuraciones -------
     BANKSEL OSCCON
     bcf ((OSCCON) and 07Fh), 4 ; Donfiguración del reloj interno
@@ -2535,36 +2587,55 @@ main:
     clrf ANSEL
     clrf ANSELH
 
+    ; ---------- Activar pines como salidas o entradas
     banksel TRISA
     bsf TRISB, B_Inc ; Colocar los pines B_Inc y B_Dec como entradas
     bsf TRISB, B_Dec
+    movlw 11110000B ; PORTA 0 al 3 como salidas y 4 al 7 como entradas
+    movwf TRISA
+    movlw 10000000B ; PORTC 0 al 6 como salidas y el 7 como entrada
+    movwf TRISC
+    movlw 10000000B ; PORTD 0 al 6 como salidas y el 7 como entrada
+    movwf TRISD
 
     banksel OPTION_REG
     bcf OPTION_REG, 7
     bsf WPUB, B_Inc ; Activar los pullups de los pines B_Inc y B_Dec
     bsf WPUB, B_Dec
 
+
+    bsf OPTION_REG, 0 ; Se selecciona un preescaler de 256
+    bsf OPTION_REG, 1
+    bsf OPTION_REG, 2
+
+
     banksel IOCB
     bsf IOCB, B_Inc ; Habilitar Interrupt on change en B_Inc y B_Dec
     bsf IOCB, B_Dec
 
-    banksel PORTA
-    movf PORTB, W
+    ;banksel PORTA
+   ; movf PORTB, W
 
 
     banksel INTCON
-    movlw 10001000B
+    movlw 11101000B
     movwf INTCON,1
     ;bsf ((INTCON) and 07Fh), 7
     ;bsf ((INTCON) and 07Fh), 3
     ;bcf ((INTCON) and 07Fh), 0
 
-
+    banksel PORTA
+    clrf PORTA
     clrf PORTB
+    clrf PORTC
+    clrf PORTD
+    clrf TMR0
 ;---------------------------------------------------------
 ;----------- Loop Forever --------------------------------
 loop:
 
     goto loop
+
+    ; 11101101B ; 237
 
 end
